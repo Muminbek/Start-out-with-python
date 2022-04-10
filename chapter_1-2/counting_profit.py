0b9b9679ac26daf0202672362d8f583a6b7de142
muminbek <mr.abidov@gmail.com>
2022-04-10 14:00:09 +0300

    nothing interesting

---
+++ chapter_1-2/counting_profit.py
@@ -0,0 +1,3 @@
+total_income =  float(input('Enter total income: '))
+profit = total_income * 0.23
+print('Your profit is $', format(profit,',.2f'), sep='')
---
+++ chapter_1-2/dollar_display.py
@@ -0,0 +1,5 @@
+monthly_pay = 30000.0
+annual_pay  = monthly_pay * 12
+print('Baшa годовая заработная плата составляет $', 
+      format(annual_pay, ',.2f'), 
+      sep='')
